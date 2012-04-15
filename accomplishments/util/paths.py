9e0be7e87a96f836b7746e4bd3679d26b89de8cf
Rafał Cieślak <rafalcieslak256@gmail.com>
2012-04-15 15:34:14 +0200

    recognising and using the installation prefix

--- accomplishments/util/paths.py
+++ accomplishments/util/paths.py
@@ -1,16 +1,47 @@
 import os
 from accomplishments.util import get_data_path
 
-def uses_system_lib():
-    return os.path.dirname(__file__).startswith("/usr")
 
-runs_in_branch = not uses_system_lib()
-#systemdata_dir = "data/daemon/"
-#media_dir      = "data/media/"
-if not runs_in_branch:
-    systemdata_dir = "/usr/share/accomplishments-daemon/"
-    media_dir      = "/usr/share/accomplishments-daemon/media/"
+if __file__[0] is '/':
+    #the script run with absolute path
+    scriptpath = __file__
 else:
-    localdatapath = get_data_path()
-    systemdata_dir = os.path.join(localdatapath, "daemon")
-    media_dir      = os.path.join(localdatapath, "media")
+    scriptpath = os.path.join(os.getcwd(),__file__)
+    
+# okay, so as we have the full path, we're ready to analise it...    
+
+# start by looking 2 levels higher. This is either the branch directory,
+# or the python2.7/...-packages directory, if the application is run installed.
+branchpath = os.path.split(os.path.split(os.path.split(scriptpath)[0])[0])[0]
+branchdir_name = os.path.split(branchpath)[1]
+if ((branchdir_name == "site-packages") or (branchdir_name == 'dist-packages')):
+    installed = True
+else:
+    installed = False
+
+# Great, at that level we know whether the application is run from branch, or
+# has been installed. This allows us to interpret the directories further...
+
+
+if installed:
+    basepath = os.path.split(os.path.split(os.path.split(branchpath)[0])[0])[0]
+    # basepath should equal to prefix given on installation.
+    print "Daemon seems to be installed to: " + basepath
+    
+    # finally, setting these significant data directories...
+    systemdata_dir = os.path.join(basepath, 'share/accomplishments-daemon')
+    media_dir      = os.path.join(basepath, 'share/accomplishments-daemon/media')
+    # these two may need to be set for the accomplismhents scripts, so that they
+    # can use OUR accomplishments module, using this installation.
+    module_dir1    = os.path.join(basepath, 'lib/python2.7/site-packages') 
+    module_dir2    = os.path.join(basepath, 'lib/python2.7/dist-packages')
+else:
+    # using branch root directory as the base path
+    basepath = branchpath
+    print "Daemon seems to be run not installed, branch base path used: " + basepath
+    
+    # finally, setting these significant data directories...
+    systemdata_dir = os.path.join(basepath, 'data/daemon')
+    media_dir      = os.path.join(basepath, 'data/media')
+    module_dir1    = None # always using default
+    module_dir2    = None # always using default
